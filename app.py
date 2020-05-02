fe06f5a2b7a65e30f5d4b3b954caf515e4770523
Sean <seanjcarley@outlook.com>
2020-05-02 13:48:16 +0100

    Updated app.py to run landing.html, added base files to static and templates folders

--- app.py
+++ app.py
@@ -2,16 +2,25 @@
 
 import os
 from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo, pymongo
-from bson.objectid import ObjectId
-import env as config
+# from flask_pymongo import PyMongo, pymongo
+# from bson.objectid import ObjectId
+# import env as config
 
 app = Flask(__name__)
 
-app.config[] = ''
-app.config[] = ''
+# app.config[] = ''
+# app.config[] = ''
 
-mongo = PyMongo(app)
+# mongo = PyMongo(app)
+
+# @app.route('/')
+# def hello():
+#     return 'Hello world ...again!'
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("landing.html")
 
 if __name__ == '__main__':
     app.run(
