3ca8d46d2dacfb39079aca76f9edfe499cc1077e
Sean <seanjcarley@outlook.com>
2020-05-03 22:11:43 +0100

    added various html files and updated existin html and py files

--- app.py
+++ app.py
@@ -2,29 +2,45 @@
 
 import os
 from flask import Flask, render_template, redirect, request, url_for
-# from flask_pymongo import PyMongo, pymongo
-# from bson.objectid import ObjectId
-# import env as config
+from flask_pymongo import PyMongo, pymongo
+from bson.objectid import ObjectId
+import env as config
 
 app = Flask(__name__)
 
-# app.config[] = ''
-# app.config[] = ''
+app.config["MONGO_DBNAME"] = 'milestone3'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
-# mongo = PyMongo(app)
-
-# @app.route('/')
-# def hello():
-#     return 'Hello world ...again!'
+mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/home')
-def home():
-    return render_template("landing.html")
+@app.route('/home/<list_id>')
+def home(list_id = ObjectId("000000000000000000000000")):
+    init_list = mongo.db.lists.find_one({"_id": ObjectId(list_id)})
+    return render_template("landing.html", lists=mongo.db.lists.find(),
+        ilist=init_list)
+    # if list_id == ObjectId("000000000000000000000000"):
+    #     return render_template("landing.html", lists=mongo.db.lists.find())
+    # else:
+    #     return render_template("landing.html", lists=mongo.db.lists.find(),
+    #         ilist=init_list)
+
+
+@app.route('/signupin')
+def signupin():
+    return render_template("signupin.html")
+
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
 
 @app.route('/login')
 def login():
     return render_template("login.html")
+
 
 if __name__ == '__main__':
     app.run(
