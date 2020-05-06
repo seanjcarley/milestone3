8d707a494c8b32fa854dd2131ddd8dc63a6ac5e7
Sean <seanjcarley@outlook.com>
2020-05-06 21:44:27 +0100

    added Add_aitems html and added add items function to app.py

--- app.py
+++ app.py
@@ -45,16 +45,15 @@ def insert_list():
         list1 = lists.find({"created":created})
         list_dict = {}
         cat_dict = {}
-        category = ""
         for i in list1:
             for k, v in i.items():
                 if k == "categories":
                     list_dict[k] = v
                     category = v
-                    print(category)
+                elif k == "_id":
+                    obj = v
                 else:
                     list_dict[k] = v
-                    print(category)
 
         cat = categories.find({"cat_name": category}, {"fields"})
 
@@ -63,19 +62,75 @@ def insert_list():
                 if k == "fields":
                     cat_dict[k] = v
 
-        return(list_dict, cat_dict)
+        return(list_dict, cat_dict, obj)
 
     the_list = set_list(now)
-    print(the_list)
+
     return render_template("add_items.html",
         the_list=the_list[0],
-        the_cat=the_list[1]
+        the_cat=the_list[1],
+        obid = the_list[2]
         )
 
 
-@app.route('/add_items', methods=['POST'])
-def add_items():
-    return redirect(url_for("home"))
+@app.route('/add_items/<obj>', methods=['POST', 'GET'])
+def add_items(obj):
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    list_id = ObjectId(obj)
+
+    imd_data = request.form
+    data = imd_data.to_dict()
+
+    items = mongo.db.items
+    lists = mongo.db.lists
+    categories = mongo.db.categories
+
+    items.insert({"list_id": list_id, "items": data, "created": now})
+
+    list1 = lists.find({"_id":list_id})
+    list_dict = {}
+    cat_dict = {}
+    itm_dict = {}
+    def set_list(created):
+        for i in list1:
+            for k, v in i.items():
+                if k == "categories":
+                    list_dict[k] = v
+                    category = v
+                elif k == "_id":
+                    obj = v
+                else:
+                    list_dict[k] = v
+
+        cat = categories.find({"cat_name": category}, {"fields"})
+        for j in cat:
+            for k, v in j.items():
+                if k == "fields":
+                    cat_dict[k] = v
+
+        itm = items.find({"list_id": list_id})
+        for m in itm:
+            for k, v in m:
+                if k == "items":
+                    for l, w in v:
+                        itm_dict[l] = w
+
+        return(list_dict, cat_dict, obj, itm_dict)
+
+
+    the_list = set_list(now)
+
+    return render_template("add_aitems.html",
+        the_list=the_list[0],
+        the_cat=the_list[1],
+        obid = the_list[2],
+        the_itm = the_list[3]
+        )
+
+
+@app.route('/finish_items')
+def finish_items():
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
