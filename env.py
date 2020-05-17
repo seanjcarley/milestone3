06f7fd3ff0ffbbc5bb08cb578628b0e507e99198
seanjcarley <seanjcarley@outlook.com>
2020-05-17 13:53:54 +0000

    cleaned up env.py

--- env.py
+++ env.py
@@ -1,3 +1,3 @@
 import os
 
-os.environ["MONGO_URI"] = 'mongodb+srv://root:sJ018316995c@my-first-cluster-3cror.mongodb.net/milestone3?retryWrites=true&w=majority'
+os.environ["MONGO_URI"] = 'mongodb+srv://root:PASSWORDREQUIRED@my-first-cluster-3cror.mongodb.net/milestone3?retryWrites=true&w=majority'
